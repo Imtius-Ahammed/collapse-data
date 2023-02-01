ee80180df4021afa84cb9396d912af91254a90b0
Imtius-Ahammed <ahammedimtius098@gmail.com>
2023-02-01 19:36:30 +0600

    format data

--- src/pages/Home/Home/Home.js
+++ src/pages/Home/Home/Home.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from 'react';
+import React, { useContext, } from 'react';
 import { AccordionContext } from 'react-bootstrap';
 import Accordion from 'react-bootstrap/Accordion';
 import { useAccordionButton } from 'react-bootstrap/AccordionButton';
